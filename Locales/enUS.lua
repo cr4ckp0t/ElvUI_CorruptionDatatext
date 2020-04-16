89ee1aa297ebd1216fe677e9f8f986fa1fd6f5ce
Adam Koch <me@adamko.ch>
2020-04-16 19:31:40 -0400

    Added summary to tooltip similar to Corruption Tooltips.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -5,4 +5,4 @@ local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
 local L = AceLocale:NewLocale("ElvUI_CorruptionDT", "enUS", true, true)
 if not L then return end
 
---@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, escape-non-ascii=true, handle-unlocalized="english")@
